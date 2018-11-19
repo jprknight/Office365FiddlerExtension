e10794788c88f327d77787843c316c7f9ea57073
Jeremy Knight <jeknight@microsoft.com>
2018-11-19 16:53:23 -0500

    Update Preferences.cs

--- EXOFiddlerInspector/Preferences.cs
+++ EXOFiddlerInspector/Preferences.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace EXOFiddlerInspector
 {
+    /// <summary>
+    /// Why preferences.cs? There are also Fiddler application preferences, why does this exist?
+    /// Fiddler application preferences are set with for example:
+    ///     FiddlerApplication.Prefs.SetBoolPref("extensions.EXOFiddlerInspector.DemoMode", false);
+    /// The preferences here are developer created preferences. These preferences are set here, are not changed
+    /// at runtime, and are hardcoded in compiled code.
+    /// </summary>
     class Preferences
     {
         /////////////////
