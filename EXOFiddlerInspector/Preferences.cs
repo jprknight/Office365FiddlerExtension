cc5dff68128abcb0f70820687f69722efad23494
Jeremy Knight <jeknight@microsoft.com>
2018-12-05 21:06:13 -0500

    1.0.60

--- EXOFiddlerInspector/Preferences.cs
+++ EXOFiddlerInspector/Preferences.cs
@@ -20,7 +20,7 @@ namespace EXOFiddlerInspector
         /// Developer Demo Mode. If enabled as much domain specific information as possible will be replaced with contoso.com.
         /// Note: This is not much right now, just outputs in response comments on the response inspector tab.
         /// </summary>
-        Boolean DeveloperDemoMode = true;
+        Boolean DeveloperDemoMode = false;
         Boolean DeveloperDemoModeBreakScenarios = false;
         /////////////////
 
