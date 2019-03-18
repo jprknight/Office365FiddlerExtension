6de5ab135032542388982aa50e0ceb4087a5f835
Jeremy Knight <40891654+jprknight@users.noreply.github.com>
2019-03-18 16:48:49 -0400

    Creating Beta Release 8103

--- EXOFiddlerInspector/Properties/AssemblyInfo.cs
+++ EXOFiddlerInspector/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5103")]
-[assembly: AssemblyFileVersion("1.0.5103")]
+[assembly: AssemblyVersion("1.0.8103")]
+[assembly: AssemblyFileVersion("1.0.8103")]
 
 [assembly: Fiddler.RequiredVersion("4.4.5.1")]
