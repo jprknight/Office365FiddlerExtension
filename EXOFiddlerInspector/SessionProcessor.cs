1aba687cb671b2a7e685b117f158b905db8029f8
Paul Arnold <paularn@microsoft.com>
2019-07-23 18:15:48 +0100

    Remove unused using

--- EXOFiddlerInspector/SessionProcessor.cs
+++ EXOFiddlerInspector/SessionProcessor.cs
@@ -7,7 +7,6 @@ using System.Net;
 using System.Collections.Generic;
 using System.Diagnostics;
 using EXOFiddlerInspector.Services;
-using System.Text.RegularExpressions;
 
 namespace EXOFiddlerInspector
 {
