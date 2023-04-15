0a88c445fce9b89e2eb51cf636d64c54bab52e92
Jeremy Knight <jeknight@microsoft.com>
2023-04-14 21:59:53 -0400

    .

--- Office365FiddlerInspector/Ruleset/HTTP_400s/Minor/HTTP_426.cs
+++ Office365FiddlerInspector/Ruleset/HTTP_400s/Minor/HTTP_426.cs
@@ -16,12 +16,16 @@ namespace Office365FiddlerInspector.Ruleset
         {
             this.session = session;
 
-            this.session["X-ResponseAlert"] = "HTTP 426 Upgrade Required.";
-            getSetSessionFlags.SetXResponseCommentsNoKnownIssue(this.session);
-
             FiddlerApplication.Log.LogString("Office365FiddlerExtension: " + this.session.id + " HTTP 426 Upgrade Required.");
 
-            this.session["X-ResponseCodeDescription"] = "426 Upgrade Required";
+            // Setting to gray, to be convinced these are important to Microsoft 365 traffic.
+            getSetSessionFlags.SetUIBackColour(this.session, "Gray");
+            getSetSessionFlags.SetUITextColour(this.session, "Black");
+
+            getSetSessionFlags.SetResponseCodeDescription(this.session, "426 Upgrade Required");
+
+            getSetSessionFlags.SetXResponseAlert(this.session, "HTTP 426 Upgrade Required.");
+            getSetSessionFlags.SetXResponseCommentsNoKnownIssue(this.session);
 
             // Nothing meaningful here, let further processing try to pick up something.
             getSetSessionFlags.SetSessionAuthenticationConfidenceLevel(this.session, "0");
--- Office365FiddlerInspector/Ruleset/HTTP_400s/Minor/HTTP_428.cs
+++ Office365FiddlerInspector/Ruleset/HTTP_400s/Minor/HTTP_428.cs
@@ -16,12 +16,16 @@ namespace Office365FiddlerInspector.Ruleset
         {
             this.session = session;
 
-            this.session["X-ResponseAlert"] = "HTTP 428 Precondition Required (RFC 6585).";
-            getSetSessionFlags.SetXResponseCommentsNoKnownIssue(this.session);
-
             FiddlerApplication.Log.LogString("Office365FiddlerExtension: " + this.session.id + " HTTP 428 Precondition Required (RFC 6585).");
 
-            this.session["X-ResponseCodeDescription"] = "428 Precondition Required (RFC 6585)";
+            // Setting to gray, to be convinced these are important to Microsoft 365 traffic.
+            getSetSessionFlags.SetUIBackColour(this.session, "Gray");
+            getSetSessionFlags.SetUITextColour(this.session, "Black");
+
+            getSetSessionFlags.SetResponseCodeDescription(this.session, "428 Precondition Required (RFC 6585)");
+
+            getSetSessionFlags.SetXResponseAlert(this.session, "HTTP 428 Precondition Required (RFC 6585).");
+            getSetSessionFlags.SetXResponseCommentsNoKnownIssue(this.session);
 
             // Nothing meaningful here, let further processing try to pick up something.
             getSetSessionFlags.SetSessionAuthenticationConfidenceLevel(this.session, "0");
