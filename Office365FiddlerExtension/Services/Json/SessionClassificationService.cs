1277547494003fcd01105989164bcd06fe9e5f56
Jeremy Knight <jeknight@microsoft.com>
2023-08-11 17:25:16 -0400

    Update SessionClassificationService.cs

--- Office365FiddlerExtension/Services/Json/SessionClassificationService.cs
+++ Office365FiddlerExtension/Services/Json/SessionClassificationService.cs
@@ -2,7 +2,6 @@
 using Fiddler.WebFormats;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Office365FiddlerExtension.Services;
 using System;
 using System.Collections.Generic;
 using System.IO;
